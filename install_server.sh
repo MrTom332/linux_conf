43276a8cacc606758606de06c48d3b6ce1a1af23
Tomás M <81438736+MrTom332@users.noreply.github.com>
2022-04-05 16:53:10 -0300

    Create install_server.sh

---
+++ install_server.sh
@@ -0,0 +1,11 @@
+apt list --upgradable
+apt upgrade
+apt update
+apt install apache2
+apt install mysql-server
+mysql_secure_installation
+
+#Configurar usuario root de mysql con nueva contraseña
+
+apt install php libapache2-mod-php
+
